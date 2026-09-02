7ded404171c7a3902919900d424a898165ed693b
Joao Souza <jujpsouza@gmail.com>
2026-09-01 23:31:12 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/io/github/joaojps/springbank/SpringbankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaojps.springbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
