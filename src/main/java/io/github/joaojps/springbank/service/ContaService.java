1070cf173a15a051f4597b3f7b8a297010cb995b
Joao Souza <jujpsouza@gmail.com>
2026-09-03 00:47:40 -0300

    feat: configuração da criação de contas e lançando primeira conta no banco de dados

--- src/main/java/io/github/joaojps/springbank/service/ContaService.java
+++ src/main/java/io/github/joaojps/springbank/service/ContaService.java
@@ -1,7 +1,11 @@
 package io.github.joaojps.springbank.service;
 
+import io.github.joaojps.springbank.model.Conta;
 import io.github.joaojps.springbank.repository.ContaRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Random;
 
 @Service
 public class ContaService {
@@ -10,5 +14,20 @@ public class ContaService {
 
     public ContaService(ContaRepository contaRepository) {
         this.contaRepository = contaRepository;
+    }
+
+    public Conta criarConta(String nomeTitular, BigDecimal saldo) {
+
+        Conta conta = new Conta(nomeTitular, saldo);
+        conta.setNumeroConta(gerarNumeroConta());
+
+        contaRepository.save(conta);
+        return conta;
+    }
+
+    private String gerarNumeroConta() {
+        Random random = new Random();
+        int numeroConta = random.nextInt(100000, 1000000);
+        return String.valueOf(numeroConta);
     }
 }
